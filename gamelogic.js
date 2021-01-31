da1fc66bc07f07f17b7416b8d463c1af9a91c70b
CoderIllusionist <75251048+CoderIllusionist@users.noreply.github.com>
2021-01-31 11:40:03 +0100

    Initial commit

--- gamelogic.js
+++ gamelogic.js
@@ -1,5 +1,24 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+  let splitWord = word.split("");
+  let placeHolder = "";
+
+  for (let i = 0; i < splitWord.length; i++) {
+    const character = splitWord[i]
+
+
+    const checkGuessedLetter = guesses.includes(character);
+    if(checkGuessedLetter === false) {
+      placeHolder = placeHolder.concat("_ ");
+    } else if (checkGuessedLetter === true) {
+      placeHolder.replace(/\s/g, '');
+      placeHolder = placeHolder.concat(character);
+    }
+
+  }
+
+  return placeHolder;
+
+
 }
 
 function isGameWon(word, guesses) {
@@ -9,9 +28,19 @@ function isGameWon(word, guesses) {
 function isGameLost(word, guesses) {
   // WRITE ME
 }
+function checkCharacter(letter) {
+
+    if (letter.length > 1) {
+      return false;
+    } else if(letter.length == 1) {
+      return  true;
+    }
+
+}
 
 module.exports = {
   displayWordSoFar: displayWordSoFar,
   isGameWon: isGameWon,
   isGameLost: isGameLost,
+  checkCharacter: checkCharacter,
 };
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const { question } = require("readline-sync");
-const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
+const { displayWordSoFar, isGameWon, isGameLost, checkCharacter } = require("./gamelogic");
 
 function game(word, guesses) {
-  console.log("Dit heb je tot nu toe geraden: ", guesses);
+  if (typeof guesses !== undefined && guesses.length ) { // if array is not empty, aka game has started, only then display this.
+    console.log("Dit heb je tot nu toe geraden: ", guesses);
+  }
 
+  const displayWord = displayWordSoFar(word, guesses)
+  console.log(displayWord);
   const letter = question("Raad een letter: ");
 
+
+
+  if(checkCharacter(letter)) {
+    guesses.push(letter);
+  } else if(!checkCharacter(letter)) {
+
+    console.log("Je mag maar met 1 letter raden")
+  }
+
   // voeg de geraden letter toe aan de array met guesses
-  guesses.push(letter);
+
 
   // volgende ronde! we roepen game nog een keer aan
   game(word, guesses);
