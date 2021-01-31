05d1e1bc231442a0ab72ac1fffa508b7d8023aa2
CoderIllusionist <75251048+CoderIllusionist@users.noreply.github.com>
2021-01-31 14:49:51 +0100

    IsGameWon & isGameLost

--- gamelogic.js
+++ gamelogic.js
@@ -1,46 +1,91 @@
 function displayWordSoFar(word, guesses) {
-  let splitWord = word.split("");
-  let placeHolder = "";
-
-  for (let i = 0; i < splitWord.length; i++) {
-    const character = splitWord[i]
+    let splitWord = word.split("");
+    let placeHolder = "";
 
 
-    const checkGuessedLetter = guesses.includes(character);
-    if(checkGuessedLetter === false) {
-      placeHolder = placeHolder.concat("_ ");
-    } else if (checkGuessedLetter === true) {
-      placeHolder.replace(/\s/g, '');
-      placeHolder = placeHolder.concat(character);
+    for (let i = 0; i < splitWord.length; i++) {
+        const character = splitWord[i].toLowerCase();
+        const guessesLower = guesses.map(toLower);
+
+        const checkGuessedLetter = guessesLower.includes(character);
+
+        if (checkGuessedLetter === false) {
+            placeHolder = placeHolder.concat("_ ");
+        } else if (checkGuessedLetter === true) {
+            placeHolder = placeHolder.concat(character) + " ";
+        }
+
     }
 
-  }
-
-  return placeHolder;
+    return placeHolder;
 
 
 }
 
-function isGameWon(word, guesses) {
-  // WRITE ME
+function isGameWon(guesses, word) {
+    guesses = guesses.join().replace(/,/g, '');
+    const wordLength = word.length;
+    let isCorrectWord = 0;
+
+    for (let i = 0; i < guesses.length; i++) {
+        if (word.includes(guesses[i])) {
+
+            let countDuplicatedCharacters = (word.match(new RegExp(guesses[i], "g")) || []).length;
+            isCorrectWord = isCorrectWord + countDuplicatedCharacters;
+            console.log(isCorrectWord)
+            console.log(wordLength)
+        }
+
+
+    }
+    if (wordLength === isCorrectWord) {
+        return true
+    } else {
+        return false;
+    }
 }
+
+function removeWrongLetters(guesses, ignoreWrongLetters) {
+
+
+}
+
 
 function isGameLost(word, guesses) {
-  // WRITE ME
+    if (guesses.length >= 7) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
+
 function checkCharacter(letter) {
 
     if (letter.length > 1) {
-      return false;
-    } else if(letter.length == 1) {
-      return  true;
+        return false;
+    } else if (letter.length == 1) {
+        return true;
     }
 
 }
 
+function toLower(guesses) {
+    return guesses.toLowerCase();
+}
+
+function removeDuplicateCharacters(placeHolder) {
+    return placeHolder
+        .split('')
+        .filter(function (item, pos, self) {
+            return self.indexOf(item) == pos;
+        })
+        .join('');
+}
+
 module.exports = {
-  displayWordSoFar: displayWordSoFar,
-  isGameWon: isGameWon,
-  isGameLost: isGameLost,
-  checkCharacter: checkCharacter,
+    displayWordSoFar: displayWordSoFar,
+    isGameWon: isGameWon,
+    isGameLost: isGameLost,
+    checkCharacter: checkCharacter,
 };
--- index.js
+++ index.js
@@ -5,17 +5,18 @@ function game(word, guesses) {
   if (typeof guesses !== undefined && guesses.length ) { // if array is not empty, aka game has started, only then display this.
     console.log("Dit heb je tot nu toe geraden: ", guesses);
   }
-
   const displayWord = displayWordSoFar(word, guesses)
-  console.log(displayWord);
-  const letter = question("Raad een letter: ");
+  const checkWon = isGameWon(guesses, word)
+  const checkLost = isGameLost(word, guesses)
+  console.log(checkWon)
+  console.log(displayWord)
+  const letter = question("Raad een letter: ").toLowerCase();
 
 
 
-  if(checkCharacter(letter)) {
+  if(checkCharacter(letter) && !guesses.includes(letter)) {
     guesses.push(letter);
   } else if(!checkCharacter(letter)) {
-
     console.log("Je mag maar met 1 letter raden")
   }
 
